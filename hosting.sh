ff38adcd1b94f40eed88a81e27f2f08a65a2644f
Amir Shams <131550204+AmirShams-ir@users.noreply.github.com>
2026-02-11 00:41:17 +0330

    Update hosting.sh

--- hosting.sh
+++ hosting.sh
@@ -232,7 +232,6 @@ detect_services() {
 # CREATE HOST
 # ==============================================================================
 create_host() {
-  detect_services
   
 title "🚀 Create Hosting Account"
 
@@ -376,15 +375,17 @@ while true; do
   echo -e "${B}${C} 🚀 Mini WHM – Modern CLI${X}"
   echo -e "${B}${C}══════════════════════════════════════${X}"
   echo
-  echo -e " ${G}1${X}) Auto Install Requisites"
+  echo -e " ${G}4${X}) Detect Services"
+  echo -e " ${G}1${X}) Install Requisites"
   echo -e " ${G}2${X}) Create Host"
   echo -e " ${G}3${X}) Delete Host"
-  echo -e " ${G}4${X}) Exit"
+  echo -e " ${G}4${X}) Exit Script"
   echo
 
   read -rp "Select an option [1-4]: " CHOICE
 
   case "$CHOICE" in
+    0) detect_services ;;
     1) auto_install ;;
     2) create_host ;;
     3) delete_host ;;
